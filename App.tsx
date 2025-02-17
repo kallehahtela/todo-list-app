237d670019f680fd1f0587f9edf5b9623d6d8916
kallehahtela <sk1mb4yt@gmail.com>
2025-02-17 12:52:26 +0200

    Changed the storing data from props to context, now these data will store just for this very session not any longer. Context is way more accessible within the app without providing these same props multiple times over and over again.

--- App.tsx
+++ App.tsx
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
+
+// Navigation
 import { NavigationContainer } from '@react-navigation/native';
 import RootStack from './src/navigation/StackNavigator';
 
+// Context
+import { TaskProvider } from './src/store/TaskContext';
+
 export default function App() {
   return (
-    <NavigationContainer>
-        <StatusBar style='dark' />
-        <RootStack />
-    </NavigationContainer>
+    <TaskProvider>
+      <NavigationContainer>
+          <StatusBar style='dark' />
+          <RootStack />
+      </NavigationContainer>
+    </TaskProvider>
   );
 }
